0ae5f62db931244935ee16b675e4334e30acdf25
matheus chaves comin santana <theusccs111@outlook.com>
2018-07-01 18:41:07 -0300

    Entidades Produto, Carrinho de Compra. Juntamente com interfaces e repositorios respectivos

---
+++ RepublicManager/RepublicManager.Api/Core/Domain/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepublicManager.Api.Core.Domain
+{
+    public class Produto : Base
+    {
+        public int ProdutoId { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public int CarrinhoDeCompraId { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
