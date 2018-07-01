407b368a64059beb97b4527422b4393c9dd6ee0b
Jhonatan-de-Souza <jdsjhonatan@hotmail.com>
2018-07-01 18:45:13 -0300

    Add classe Aviso e Configuração Fluent API

---
+++ RepublicManager/RepublicManager.Api/Core/Configuration/AvisosConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RepublicManager.Api.Core.Domain;
+
+namespace RepublicManager.Api.Core.Configuration
+{
+    public class AvisosConfiguration : IEntityTypeConfiguration<Aviso>
+    {
+        public void Configure(EntityTypeBuilder<Aviso> builder)
+        {
+            //Definindo Primary Key
+            builder.HasKey(aviso => aviso.Id);
+            builder.Property(aviso => aviso.Descricao)
+                .IsRequired()
+                .HasMaxLength(250);
+            builder.Property(aviso => aviso.DataAviso)
+                .IsRequired();
+        }
+    }
+}
