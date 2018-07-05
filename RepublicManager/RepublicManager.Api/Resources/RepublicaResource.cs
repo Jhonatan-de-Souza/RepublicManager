e22a5628956954a511d681b2751f6259e6e1f667
matheus chaves comin santana <theusccs111@outlook.com>
2018-07-04 21:37:16 -0300

    testes

---
+++ RepublicManager/RepublicManager.Api/Resources/RepublicaResource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepublicManager.Api.Resources
+{
+    public class RepublicaResource
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Vagas { get; set; }
+
+        public RepublicaResource()
+        {
+            isAtivo = true;
+        }
+        public DateTime DataRegistro { get; set; }
+        public bool isAtivo { get; set; }
+        public int CriadoPor { get; set; }
+
+    }
+}
