b2d8103ac1f96972f5393978e295eb40b8121468
QUAY Sistemas e Portais <microsoft@quay.com.br>
2018-07-18 15:22:37 -0300

    Test de Republica, Getall por tipo ok. Por n resultados ainda estudando como passar objetos ao mock.

--- RepublicManager/RepublicManager.Tests/RepublicaControllerTest.cs
+++ RepublicManager/RepublicManager.Tests/RepublicaControllerTest.cs
@@ -26,16 +26,124 @@ namespace RepublicManager.Tests
             var mockUoW = new Mock<IUnitOfWork>();
             mockUoW.SetupGet(a => a.Republicas).Returns(mockRepository.Object);
             _republicaController = new RepublicaController(mockUoW.Object);
+
+
+            List<Republica> republicas = new List<Republica>();
+            republicas.Add(new  Republica()
+            {
+                Nome = "Nome Teste",
+                Vagas = 3
+            });
+
+            mockRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(republicas);            
+
         }
 
         [Fact]
-        public async Task Values_Get_All()
+        public async Task Values_GetAll_OkResult()
         {
 
             // Act
             var result = await _republicaController.GetAll();
             result.Should().BeOfType<OkObjectResult>();
 
+           /* var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var republicas = okResult.Value.Should().BeAssignableTo<List<RepublicaResource>>().Subject;
+
+
+            republicas.Count().Should().Be(0);*/
+
         }
+
+
+        [Fact]
+        public async Task Values_GetAll_N_Results()
+        {
+
+            // Act
+            var result = await _republicaController.GetAll();
+           
+
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var republicas = okResult.Value.Should().BeAssignableTo<List<RepublicaResource>>().Subject;
+
+
+            republicas.Count().Should().Be(0);
+
+        }
+
+
+        /*[Fact]
+  public async Task Persons_Add()
+  {
+    // Arrange
+    var controller = new PersonsController(new PersonService());
+    var newPerson = new Person
+    {
+      FirstName = "John",
+      LastName = "Doe",
+      Age = 50,
+      Title = "FooBar",
+      Email = "john.doe@foo.bar"
+    };
+
+    // Act
+    var result = await controller.Post(newPerson);
+
+    // Assert
+    var okResult = result.Should().BeOfType<CreatedAtActionResult>().Subject;
+    var person = okResult.Value.Should().BeAssignableTo<Person>().Subject;
+    person.Id.Should().Be(51);
+  }
+
+  [Fact]
+  public async Task Persons_Change()
+  {
+    // Arrange
+    var service = new PersonService();
+    var controller = new PersonsController(service);
+    var newPerson = new Person
+    {
+      FirstName = "John",
+      LastName = "Doe",
+      Age = 50,
+      Title = "FooBar",
+      Email = "john.doe@foo.bar"
+    };
+
+    // Act
+    var result = await controller.Put(20, newPerson);
+
+    // Assert
+    var okResult = result.Should().BeOfType<NoContentResult>().Subject;
+
+    var person = service.Get(20);
+    person.Id.Should().Be(20);
+    person.FirstName.Should().Be("John");
+    person.LastName.Should().Be("Doe");
+    person.Age.Should().Be(50);
+    person.Title.Should().Be("FooBar");
+    person.Email.Should().Be("john.doe@foo.bar");
+  }
+
+  [Fact]
+  public async Task Persons_Delete()
+  {
+    // Arrange
+    var service = new PersonService();
+    var controller = new PersonsController(service);
+
+    // Act
+    var result = await controller.Delete(20);
+
+    // Assert
+    var okResult = result.Should().BeOfType<NoContentResult>().Subject;
+
+    // should throw an eception, 
+    // because the person with id==20 doesn't exist enymore
+    AssertionExtensions.ShouldThrow<InvalidOperationException>(
+      () => service.Get(20));
+  }
+}*/
     }
 }
