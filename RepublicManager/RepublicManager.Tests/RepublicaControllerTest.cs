ab75e7ebc80916c9926642d3eced4025ae237352
matheus chaves comin santana <theusccs111@outlook.com>
2018-07-17 22:38:01 -0300

    Test Republica GetAll

--- RepublicManager/RepublicManager.Tests/RepublicaControllerTest.cs
+++ RepublicManager/RepublicManager.Tests/RepublicaControllerTest.cs
@@ -1,32 +1,41 @@
-﻿using System.Threading.Tasks;
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using RepublicManager.Api.Controllers;
 using RepublicManager.Api.Core;
 using RepublicManager.Api.Core.Domain;
 using RepublicManager.Api.Core.Repositories;
+using RepublicManager.Api.Core.Resources;
+using Xunit;
 
 namespace RepublicManager.Tests
 {
-    [TestClass]
     public class RepublicaControllerTest
     {
         private readonly RepublicaController _republicaController;
+
         public RepublicaControllerTest()
         {
+            //In order to use a mock implementation of a Unit Of work you must 
+            // first create a mock representation of it and pass it Object instance to where it needs to be used
             var mockRepository = new Mock<IRepublicaRepositorio>();
             var mockUoW = new Mock<IUnitOfWork>();
             mockUoW.SetupGet(a => a.Republicas).Returns(mockRepository.Object);
             _republicaController = new RepublicaController(mockUoW.Object);
         }
 
-        [TestMethod]
-        public async Task GetAll_returnsSuccess()
+        [Fact]
+        public async Task Values_Get_All()
         {
+
+            // Act
             var result = await _republicaController.GetAll();
-            //result.Should().
+            result.Should().BeOfType<OkObjectResult>();
+
         }
     }
 }
