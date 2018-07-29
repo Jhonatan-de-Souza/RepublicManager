0028de8338a5c76e65581c7b2b89950ce32def1d
matheus chaves comin santana <theusccs111@outlook.com>
2018-07-29 18:32:13 -0300

    Carrinho de Compra e produto ok

--- RepublicManager/RepublicManager.Api/Core/Resources/ProdutoResource.cs
+++ RepublicManager/RepublicManager.Api/Core/Resources/ProdutoResource.cs
@@ -9,7 +9,6 @@ namespace RepublicManager.Api.Core.Resources
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
         public int CarrinhoDeCompraId { get; set; }
-        public CarrinhoDeCompraResource CarrinhoDeCompra { get; set; }
         public int UsuarioId { get; set; }
 
 
